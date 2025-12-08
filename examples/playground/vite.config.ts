338f6f03a5d433002fb775a0b01f2e27c3219c38
Rajtosh Ranjan <66785808+rajtoshranjan@users.noreply.github.com>
2025-12-08 15:31:22 +0530

    Change base path in Vite config to root

--- examples/playground/vite.config.ts
+++ examples/playground/vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/use-idb-store/",
+  base: "/",
   resolve: {
     dedupe: ["react", "react-dom"],
     alias: {
